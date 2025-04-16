068611c1691c22ae379d77b0066af0958556842b
Ben Gamari <ben@smart-cactus.org>
2025-04-15 22:29:52 -0400

    Refactor as overlay

--- flake.nix
+++ flake.nix
@@ -2,21 +2,25 @@
   description = "Flake utils demo";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
+      let pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      in {
         packages = rec {
-          mailman-archive-mapper = pkgs.python3Packages.callPackage ./mapper.nix {
-            inherit (pkgs) mailmanPackages;
-          };
-          default = mailman-archive-mapper;
+          default = pkgs.mailman-archive-mapper;
         };
         apps = rec {
           mailman-archive-mapper = flake-utils.lib.mkApp { drv = self.packages.${system}.mailman-archive-mapper; };
           default = mailman-archive-mapper;
         };
-      }
-    );
+      }) // {
+        overlays.default = self2: super2: {
+          mailman-archive-mapper =
+            self2.mailmanPackages.hyperkitty.pythonModule.pkgs.callPackage ./mapper.nix {
+              inherit (self2) mailmanPackages;
+            };
+        };
+      };
 }
